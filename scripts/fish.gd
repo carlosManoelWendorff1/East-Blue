d2c50207018077c12a497ceb21f532d513a203ee
Gabriel Rosa <gabriel.edu.rosa@proton.me>
2024-11-13 20:35:38 -0300

    add rope

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 200.0
 var fish_speed = 200.0
 var caught = false
+var player = null
 
 func _physics_process(delta: float) -> void:
 	if caught:
@@ -11,16 +12,21 @@ func _physics_process(delta: float) -> void:
 			velocity.y = direction * SPEED
 		else:
 			velocity.y = move_toward(velocity.y, 0, SPEED)
-			
 		move_and_slide()
 		
 	if not caught:
 		velocity.x = fish_speed
 		move_and_slide()
 
+	if velocity.x < 0:
+		self.get_child(0).flip_h = true
+	elif velocity.x > 0:
+		self.get_child(0).flip_h = false
+
 
 func on_collision(body: Node2D):	
 	if body.is_in_group("player") && not body.has_fish:
+		player = body
 		caught = true
 		velocity.x = 0.0
 		body.has_fish = true
