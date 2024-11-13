a7fef6854e72d790e44691ac4f006eb29e0a4a83
Gabriel Rosa <gabriel.edu.rosa@proton.me>
2024-11-12 21:15:00 -0300

    fishing

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-var player_speed = 0.0;
+var fish_speed = 200.0
 var caught = false
 
 func _physics_process(delta: float) -> void:
@@ -12,15 +12,22 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.y = move_toward(velocity.y, 0, SPEED)
 			
-	move_and_slide()
-	
+		move_and_slide()
+		
 	if not caught:
+		velocity.x = fish_speed
+		move_and_slide()
 		for index in get_slide_collision_count():
 			var collision = get_slide_collision(index)
 			
 			if collision == null:
 				return
 				
-			if collision.get_collider().is_in_group("player"):
+			var coll = collision.get_collider()
+			
+			if coll.is_in_group("player"):
 				caught = true
+				velocity.x = 0.0;
+			elif coll.is_in_group("wall"):
+				fish_speed = -fish_speed
 				
