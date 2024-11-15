a2d2cd35e07536a7c5071b4862b5370824213563
Gabriel Rosa <gabriel.edu.rosa@proton.me>
2024-11-15 20:23:38 -0300

    fishing enhancements

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	if caught:
 		sprite.stop()
 		sprite.flip_v = true
-		position.y = player.position.y + 10
+		position.y = player.position.y + 30
 		position.x = player.position.x - 50
 		move_and_slide()
 		
